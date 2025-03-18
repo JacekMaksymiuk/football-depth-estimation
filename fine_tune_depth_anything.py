513a2435f51ec60524f48fb62b650014482e2263
jacek <maksymiukjacek1@gmail.com>
2025-03-19 00:16:16 +0100

    validation

--- fine_tune_depth_anything.py
+++ fine_tune_depth_anything.py
@@ -17,6 +17,15 @@ from depth_anything.dpt import DepthAnything
 from depth_anything.transform import Resize, NormalizeImage, PrepareForNet
 
 
+class RSELoss(torch.nn.Module):
+
+    def __init__(self):
+        super(RSELoss, self).__init__()
+
+    def forward(self, pred, target):
+        return torch.mean(((target - pred) ** 2) / target)
+
+
 class DepthAnythingFineTuner:
 
     def __init__(self, depth_train_path: Path, depth_val_path: Path, depth_test_path: Path):
@@ -39,13 +48,58 @@ class DepthAnythingFineTuner:
         train_loader = DataLoader(train_ds, batch_size=1, shuffle=True)
         val_loader = DataLoader(val_ds, batch_size=1, shuffle=False)
 
+        loss_fn = RSELoss()
         optimizer = optim.AdamW(self._depth_anything.parameters(), lr=5e-6, weight_decay=0.01)
         scheduler = LambdaLR(optimizer, lr_lambda=lambda e: 1.0 - (e / (n_epochs - 1)) * 0.9)
 
         for epoch in range(n_epochs):
+
+            # Train
             self._depth_anything.train()
-            for images, depths in tqdm(train_loader, desc=f'Epoch {epoch + 1}/{n_epochs}', leave=True):
+            total_loss, cnt = 0.0, 0
+            train_progress_bar = tqdm(train_loader, desc=f'Epoch {epoch + 1}/{n_epochs}', leave=True)
+            for images, depths in train_progress_bar:
                 images, depths = images.to('cuda'), depths.to('cuda')
+                predictions = self._depth_anything(images)
+                loss = loss_fn(predictions, depths)
+                loss = loss / batch_size  # New
+                cnt += 1
+                loss.backward()
+
+                if cnt % batch_size == 0 or cnt == len(train_loader):
+                    optimizer.step()
+                    optimizer.zero_grad()
+
+                total_loss += loss.item() * batch_size
+                train_progress_bar.set_postfix(loss=loss.item() * batch_size, avg_loss=total_loss / cnt)
+
+            scheduler.step()
+            print(f'Epoch {epoch + 1}/{n_epochs}, Loss: {total_loss / len(train_loader):.4f}')
+
+            ### Val
+            self._depth_anything.eval()
+            total_err, total_cnt, total_err_sq = 0.0, 0.0, 0
+            val_progress_bar = tqdm(os.listdir(self._depth_val_path), desc=f"Val error", leave=True)
+            for filename in val_progress_bar:
+                img_path = str(self._image_val_path / filename)
+                np_orig = cv2.imread(str(self._depth_val_path / filename), cv2.IMREAD_UNCHANGED)
+                h, w = np_orig.shape[:2]
+
+                with torch.no_grad():
+                    img_to_pred = DepthDataset.load_image(img_path, self._transform).unsqueeze(0).to('cuda')
+                    depth = self._depth_anything(img_to_pred)
+                depth = F.interpolate(depth[None], (h, w), mode='bilinear', align_corners=False)[0, 0]
+                depth = (depth - depth.min()) / (depth.max() - depth.min()) * 255.0 ** 2
+                depth = depth.cpu().numpy().astype(np.uint16)
+
+                scale, shift = self._compute_scale_and_shift_np(depth, np_orig)
+                depth = scale * depth + shift
+
+                total_err_sq += np.mean(((np_orig - depth) ** 2) / np_orig)
+                total_err += self._silog(depth, np_orig)
+                total_cnt += 1
+                val_progress_bar.set_postfix(silog_err=total_err / total_cnt, rse=total_err_sq / total_cnt)
+            print(f"Val loss: silog: {(total_err / total_cnt):.4f}, sq: {(total_err_sq / total_cnt):.4f}")
 
     @staticmethod
     def _prepare_transform():
@@ -103,3 +157,11 @@ class DepthAnythingFineTuner:
 
             new_np_orig_depth = new_np_orig_depth.astype(np.uint16)
             cv2.imwrite(depth_path, new_np_orig_depth)
+
+    @staticmethod
+    def _silog(pred: np.ndarray, target: np.ndarray, epsilon=1e-6):
+        pred = np.clip(pred, epsilon, None)
+        target = np.clip(target, epsilon, None)
+        log_diff = np.log(pred) - np.log(target)
+        silog_err = np.sqrt(np.mean(log_diff ** 2) - (np.mean(log_diff) ** 2))
+        return silog_err * 100
